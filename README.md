4fc0d6bbfd3fe8f24ac40429123fd47d9290f95f
star <amyzouxing@outlook.com>
2021-09-29 14:45:38 +0800

    update readme, add comments

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
-# arkworks_learn
+## Introduction
 
-Implement matrix multiplication zkSnark.
+This is an example of using [arkworks](https://github.com/arkworks-rs )' libraries to create [zksnark](https://zkp.science/).
+
+Arkworks have many great work about zksnark, including [groth16](https://github.com/arkworks-rs/groth16), and [ripp](https://github.com/arkworks-rs/ripp ) which is aggregated groth16.
+
+The prover **P** has a matrix multiplication equation,
 
 X * W = Y
 
-where X and Y are public inputs, W is the witness.
+where X and Y are public inputs, W is the witness (secret information only known to **P**).
+
+**P** could generate proof about the matrix multiplication computation.
+
+The verifier **V** could verify the proof. 
+
+If the result is true, then V could trust P.
+
+## How to run
+
+1. make sure you have **rust** environment
+
+2. run the following command in terminal
+
+   ```shell
+   cargo run
+   ```
+
+   
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
+// import libraries
 use ark_r1cs_std::{fields::fp::FpVar, alloc::AllocVar};
 use ark_r1cs_std::eq::EqGadget;
 use ark_relations::r1cs::{ConstraintSynthesizer, ConstraintSystemRef, SynthesisError}; 
 use ark_bls12_381::{Bls12_381, Fr}; 
 use ark_ff::{Zero}; 
 use ark_groth16::*;
-use ark_crypto_primitives::snark::*;
+use ark_crypto_primitives::snark::*; // import Groth16 library
 use ark_std::rand::{rngs::StdRng, SeedableRng};
 
+// rename FpVar which is a variable in Fp256
 pub type FqVar = FpVar<Fr>;
 pub type Fq = Fr;
 
-// Prove: x * w = y 
+// Prove matrix multiplication correctness: x * w = y 
+// 1. We define a struct which contains the data needed to prove the computation.
 #[derive(Clone)]
 struct MatmulCircuit {
     x: Vec<Vec<Fq>>,   // public input
@@ -18,14 +21,17 @@ struct MatmulCircuit {
     y: Vec<Vec<Fq>>,   // public input
 }
 
-impl ConstraintSynthesizer<Fq> for MatmulCircuit {
+// 2. We implement this trait and implement the details for generate_constraints function.
+impl ConstraintSynthesizer<Fq> for MatmulCircuit { 
     fn generate_constraints(self, cs: ConstraintSystemRef<Fq>) -> Result<(), SynthesisError> {
+        // get x's row length, x's col length, y's col length
         let m = self.x.len();
         let t = self.x[0].len();
         let n = self.y[0].len();
 
         for i in 0..m { 
             for j in 0..t {
+                // x is public input
                 let x_input = FpVar::<Fq>::new_input(
                     ark_relations::ns!(cs, "new input gadget"), || Ok(self.x[i][j])
                 ).expect("create new input"); 
@@ -34,29 +40,32 @@ impl ConstraintSynthesizer<Fq> for MatmulCircuit {
 
         for i in 0..t { 
             for j in 0..n {
+                // w is the witness which should be hidden
                 let w_witness = FpVar::<Fq>::new_witness(
                     ark_relations::ns!(cs, "new witness gadget"), || Ok(self.w[i][j])
                 ).expect("create new witness"); 
             }
         }
-
-         
-   
+ 
         // matrix multiplication
         for j in 0..n {
             for i in 0..m {
+                // create zero value
                 let mut tmp_sum = FpVar::<Fq>::new_witness(
                     ark_relations::ns!(cs, "zero gadget"), || Ok(Fq::zero())
                 ).expect("create zero ");
+
                 for k in 0..t {
                     // calculate the sum of x row i * w col j  
                     tmp_sum += self.x[i][k] * self.w[k][j]; 
                 }
+
+                // y is public input
                 let y_input = FpVar::<Fq>::new_input(
                     ark_relations::ns!(cs, "new input gadget"), || Ok(self.y[i][j])
                 ).expect("create new input"); 
 
-                //y_input.enforce_equal(&tmp_sum).expect("enforce equal: x * w = y");
+                y_input.enforce_equal(&tmp_sum).expect("enforce equal: x * w = y");
             }
         } 
         Ok(())
@@ -65,11 +74,14 @@ impl ConstraintSynthesizer<Fq> for MatmulCircuit {
    
 // map i64 to a finite field Fp256
 fn to_fq(x: i64) -> Fq {
+    // get the positive value of x
     let val:u64 = i64::unsigned_abs(x); 
+    // map integer to Fp256
     let mut fq: Fq = val.into();  
     if x< 0 { 
         // let modulus = ark_bls12_381::FrParameters::MODULUS;
         // println!("{:#?}", modu);
+        // if x is negative, we should return the inverse value
         fq = - fq;   // neg_fq = modulus - fq
     }  
     fq
@@ -79,24 +91,17 @@ fn to_fq(x: i64) -> Fq {
 fn main() {  
     println!("===========================  matmul circuit test  ===========================  ");
     let mut rng = StdRng::seed_from_u64(0u64);
-    let x = vec![vec![to_fq(-1); 4]; 5];   // 5*4 matrix     
-    let w = vec![vec![to_fq(2); 3]; 4];    // 4*3 matrix
-    let y = vec![vec![to_fq(-8); 3]; 5];   // 5*3 matrix
+    let x = vec![vec![to_fq(-1); 4]; 5];   // 5*4 matrix   all elements are -1  
+    let w = vec![vec![to_fq(2); 3]; 4];    // 4*3 matrix   all elements are 2
+    let y = vec![vec![to_fq(-8); 3]; 5];   // 5*3 matrix   all elements are -8
 
     let circuit = MatmulCircuit {
         x : x.clone(),
         w : w.clone(),
         y : y.clone(), 
     };
- 
-    // let params = Groth16::<Bls12_381>::setup(circuit.clone(), &mut rng).unwrap();
-    // println!("set up");
-
-    // let proof =  Groth16::<Bls12_381>::prove(&params.0, circuit.clone(), &mut rng).unwrap(); 
-    // println!("prove");
-
-    // let result = Groth16::<Bls12_381>::verify(&params.1, &statement, &proof).unwrap();
- 
+  
+    // statement: collect all public inputs
     let mut statement = Vec::new();
     for i in 0..5 {
         for j in 0..4 {
@@ -105,11 +110,10 @@ fn main() {
     }
     for i in 0..5 {
         for j in 0..3 {
-            statement.push(x[i][j].clone());
+            statement.push(y[i][j].clone()); // if we change y to x, we will get false when verify result.
         } 
     }
- 
-    
+  
     let param = generate_random_parameters::<Bls12_381, _, _>(circuit.clone(), &mut rng).unwrap(); 
     println!("setup");
 
@@ -120,5 +124,16 @@ fn main() {
 
     let result = verify_proof(&pvk, &proof, &statement).unwrap(); 
     println!("verify result is {:?}", result);
+
+    
+    // Optional: we could use Groth16 library to setup, prove and verify a circuit.
+    
+    // let params = Groth16::<Bls12_381>::setup(circuit.clone(), &mut rng).unwrap();
+    // println!("set up");
+
+    // let proof =  Groth16::<Bls12_381>::prove(&params.0, circuit.clone(), &mut rng).unwrap(); 
+    // println!("prove");
+
+    // let result = Groth16::<Bls12_381>::verify(&params.1, &statement, &proof).unwrap();
 
 }
